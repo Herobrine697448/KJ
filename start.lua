e1bc889917f352e1eb3abb48c5b85717f474d58c
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-18 11:12:39 +0300

    Update start.lua

--- start.lua
+++ start.lua
@@ -14,7 +14,7 @@ end
 resetCharacter()
 
 -- Wait for 2 seconds
-wait(5)
+wait(4.5)
 
 -- Load and execute the next script
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Herobrine697448/KJ/refs/heads/main/main.lua"))()
