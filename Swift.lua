3a567acd7fa6b09ce14916474f15334c3411aa5e
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-18 14:15:14 +0300

    Update Swift.lua

--- Swift.lua
+++ Swift.lua
@@ -1,37 +1,29 @@
-local function swiftSweep()
-    -- Set the character's walk speed to 0
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
-    
-    local player = game.Players.LocalPlayer
-    repeat wait() until player.Character.Humanoid
-    local humanoid = player.Character.Humanoid
-    local character = player.Character or player.CharacterAdded:Wait()
-    local UserInputService = game:GetService("UserInputService")
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+local player = game.Players.LocalPlayer
+repeat wait() until player.Character.Humanoid
+local humanoid = player.Character.Humanoid
+local character = player.Character or player.CharacterAdded:Wait()
+local UserInputService = game:GetService("UserInputService")
 
-    -- Define and play the sound
-    local soundId = 18654280691 -- Correct sound ID
-    local sound = Instance.new("Sound")
-    sound.Name = "Swift_Sweep" -- Name of the sound instance
-    sound.SoundId = "rbxassetid://" .. soundId
-    sound.Volume = 1
-    sound.Pitch = 1.0
-    sound.PlaybackSpeed = 1.0
-    sound.Parent = character.Torso
-    sound:Play()
-    
-    -- Define and play the animation
-    local anim2 = Instance.new("Animation")
-    anim2.AnimationId = "rbxassetid://16944345619"
-    local playAnim2 = humanoid:LoadAnimation(anim2)
-    anim2.AnimationId = "rbxassetid://0"
-    playAnim2:Play()
-    
-    -- Wait for the animation to complete
-    wait(1)
-    
-    -- Reset the character's walk speed
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
-end
+-- Create and play the sound for Swift Sweep
+local soundId = 18654280691  -- Correct sound ID
+local sound = Instance.new("Sound")
+sound.Name = "Swift_Sweep_Sound"
+sound.SoundId = "rbxassetid://" .. soundId
+sound.Volume = 1
+sound.Pitch = 1.0
+sound.PlaybackSpeed = 1.0
+sound.Parent = character:FindFirstChild("HumanoidRootPart")  -- Parent to HumanoidRootPart or any other part of the character
+sound:Play()
+
+local anim2 = Instance.new("Animation")
+anim2.AnimationId = "rbxassetid://16944345619"
+
+local playAnim2 = humanoid:LoadAnimation(anim2)
+anim2.AnimationId = "rbxassetid://0"
+playAnim2:Play()
+wait(1)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 
 --Dummy Animation
 local player = game.Workspace.Live["Weakest Dummy"]
