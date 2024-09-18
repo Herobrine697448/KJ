481aeb0f4070ec29c5380d5e725dc6e0b25419b9
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-18 13:10:46 +0300

    Update Swift.lua

--- Swift.lua
+++ Swift.lua
@@ -1,1 +1,26 @@
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
+local player = game.Players.LocalPlayer
+repeat wait() until player.Character.Humanoid
+local humanoid = player.Character.Humanoid
+local character = player.Character or player.CharacterAdded:Wait()
+local UserInputService = game:GetService("UserInputService")
 
+local anim2 = Instance.new("Animation")
+anim2.AnimationId = "rbxassetid://16944345619"
+
+local playAnim2 = humanoid:LoadAnimation(anim2)
+anim2.AnimationId = "rbxassetid://0"
+playAnim2:Play()
+wait(1)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
+
+--Dummy Animation
+local player = game.Workspace.Live["Weakest Dummy"]
+repeat wait() until player.Humanoid
+local dummyhumanoid = game.Workspace.Live["Weakest Dummy"]["Humanoid"]
+
+local anim2 = Instance.new("Animation")
+anim2.AnimationId = "rbxassetid://10471478869"
+
+local playAnim2 = dummyhumanoid:LoadAnimation(anim2)
+playAnim2:Play()lay()
