133c113315834c81f013dc3075449a057d4bc72f
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-18 14:42:32 +0300

    Update Swift.lua

--- Swift.lua
+++ Swift.lua
@@ -1,29 +1,4 @@
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 0
-local player = game.Players.LocalPlayer
-repeat wait() until player.Character.Humanoid
-local humanoid = player.Character.Humanoid
-local character = player.Character or player.CharacterAdded:Wait()
-local UserInputService = game:GetService("UserInputService")
 
--- Create and play the sound for Swift Sweep
-local soundId = 18654280691  -- Correct sound ID
-local sound = Instance.new("Sound")
-sound.Name = "Swift_Sweep_Sound"
-sound.SoundId = "rbxassetid://" .. soundId
-sound.Volume = 1
-sound.Pitch = 1.0
-sound.PlaybackSpeed = 1.0
-sound.Parent = character:FindFirstChild("HumanoidRootPart")  -- Parent to HumanoidRootPart or any other part of the character
-sound:Play()
-
-local anim2 = Instance.new("Animation")
-anim2.AnimationId = "rbxassetid://16944345619"
-
-local playAnim2 = humanoid:LoadAnimation(anim2)
-anim2.AnimationId = "rbxassetid://0"
-playAnim2:Play()
-wait(1)
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 
 --Dummy Animation
 local player = game.Workspace.Live["Weakest Dummy"]
@@ -32,6 +7,5 @@ local dummyhumanoid = game.Workspace.Live["Weakest Dummy"]["Humanoid"]
 
 local anim2 = Instance.new("Animation")
 anim2.AnimationId = "rbxassetid://10471478869"
-
 local playAnim2 = dummyhumanoid:LoadAnimation(anim2)
 playAnim2:Play()lay()
