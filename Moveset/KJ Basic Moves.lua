cb9e6de2b35b35396f9a9fb70d8c8b15f037ed22
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-29 18:27:46 +0300

    Delete Moveset directory
    Signed-off-by: Herobrine697448 <dalesisgiorgos@gmail.com>

--- Moveset/KJ Basic Moves.lua
+++
@@ -1,9 +0,0 @@
-local player = game.Players.LocalPlayer
-local backpack = player.Backpack
-
-for _, item in ipairs(backpack:GetChildren()) do
-    if item:IsA("Tool") then
-        item:Destroy()
-    end
-end
-
