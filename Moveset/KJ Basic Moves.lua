ca5e708a98c24966509b43d7fe5c34608be7cfc4
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-09-29 18:25:57 +0300

    Update KJ Basic Moves.lua
    Signed-off-by: Herobrine697448 <dalesisgiorgos@gmail.com>

--- Moveset/KJ Basic Moves.lua
+++ Moveset/KJ Basic Moves.lua
@@ -1,1 +1,9 @@
+local player = game.Players.LocalPlayer
+local backpack = player.Backpack
+
+for _, item in ipairs(backpack:GetChildren()) do
+    if item:IsA("Tool") then
+        item:Destroy()
+    end
+end
 
