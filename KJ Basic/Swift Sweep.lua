ca25302ec5e55fb53ba6d8f7f4525a1a34c564bf
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-18 22:44:44 +0300

    Update Swift Sweep.lua
    Signed-off-by: Herobrine697448 <dalesisgiorgos@gmail.com>

--- KJ Basic/Swift Sweep.lua
+++ KJ Basic/Swift Sweep.lua
@@ -1,7 +1,9 @@
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 10
 local player = game.Players.LocalPlayer
 repeat wait() until player.Character.Humanoid
 local humanoid = player.Character.Humanoid
 local character = player.Character or player.CharacterAdded:Wait()
+local UserInputService = game:GetService("UserInputService")
 
 local anim2 = Instance.new("Animation")
 anim2.AnimationId = "rbxassetid://16944345619"
@@ -9,6 +11,8 @@ anim2.AnimationId = "rbxassetid://16944345619"
 local playAnim2 = humanoid:LoadAnimation(anim2)
 anim2.AnimationId = "rbxassetid://0"
 playAnim2:Play()
+wait(1)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 16
 
 local function enableParticleEmitters(parent)
     for _, descendant in ipairs(parent:GetDescendants()) do
