74452cda7c8260a51cdd42b2876a2301351ba103
Herobrine697448 <dalesisgiorgos@gmail.com>
2024-10-19 01:22:08 +0300

    Update Awakening.lua
    Signed-off-by: Herobrine697448 <dalesisgiorgos@gmail.com>

--- Awakenings/Awakening.lua
+++ Awakenings/Awakening.lua
@@ -146,7 +146,7 @@ tool.Parent = game.Players.LocalPlayer.Backpack
  
 -- Function to execute when the tool is activated
 local function onActivated()
-    loadstring(game:HttpGet(""))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Herobrine697448/KJ/refs/heads/main/Ultimate/Five%20Seasons.lua"))()
 end
  
 -- Connect the activation function to the tool's Activated event
